c0abf94284b8416d1e8c18a8a9397b19d12aeac1
NekichNet <nikitanekich2017@gmail.com>
2025-11-22 16:26:35 +0500

    Модели сделаны #5

---
+++ server/server.Domain/Models/Course.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public List<Skill> Skills { get; set; }
+        public List<Profession> Professions { get; set; }
+
+        public Course()
+        {
+        }
+
+        public Course(int id, string name, string description, string imagePath)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            ImagePath = imagePath;
+        }
+    }
+}
