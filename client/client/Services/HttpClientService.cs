2614c6a5bb7b5073cf9ce06f36994562407449ab
Yupi <youngperchik2404@gmail.com>
2025-11-22 19:11:19 +0500

    написан LoginViewModel #4

---
+++ client/client/Services/HttpClientService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Services
+{
+    public class HttpClientService
+    {
+        public HttpClient HttpClient { get; set; }
+        public HttpClientService() 
+        {
+            HttpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:8080/api/")
+            };
+        }
+    }
+}
