c7f2a427909cadc59d987c4c91f1af8594a88b68
Dasadn0 <Yakaleks96@gmail.com>
2025-11-23 00:39:53 +0500

    ProfessionRepository

---
+++ server/server.Infrastucture/Repositories/CourseRepository.cs
@@ -0,0 +1,35 @@
+﻿using server.Domain.Interfaces.Repositories;
+using server.Domain.Models;
+using server.Persistence;
+using server.Persistence.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Infrastucture.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public async Task<List<Course>> GetAllCourses()
+        {
+            return _context.Courses.ToList();
+        }
+        public async Task<Course> GetCourse(int id)
+        {
+            return _context.Courses.ElementAt(id);
+        }
+        public async Task<List<Profession>> GetCourseProfessions(int id)
+        {
+            return _context.Courses.ElementAt(id).Professions;
+        }
+        public async Task<List<LeveledSkill>> GetCourseSkills(int id)
+        {
+            return _context.Courses.ElementAt(id).Skills;
+        }
+    }
+}
---
+++ server/server.Infrastucture/Repositories/ProfessionRepository.cs
@@ -0,0 +1,35 @@
+﻿using server.Domain.Interfaces.Repositories;
+using server.Domain.Models;
+using server.Persistence;
+using server.Persistence.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Infrastucture.Repositories
+{
+    public class ProfessionRepository : IProfessionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public async Task<List<Profession>> GetAllProfessions()
+        {
+            return _context.Professions.ToList();
+        }
+        public async Task<Profession> GetProfession(int id)
+        {
+            return _context.Professions.ElementAt(id);
+        }
+        public async Task<List<Course>> GetProfessionCourses(int id)
+        {
+            return _context.Professions.ElementAt(id).Courses;
+        }
+        public async Task<List<Skill>> GetProfessionSkills(int id)
+        {
+            return _context.Professions.ElementAt(id).Skills;
+        }
+    }
+}
