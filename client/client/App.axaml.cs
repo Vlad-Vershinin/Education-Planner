643016fde4c64015185d5ce35e1cf317b23b2f4b
Yarik Vodila <109005417+RiftSquadronFounder@users.noreply.github.com>
2025-11-22 11:47:18 +0500

    Setup

--- client/client/App.axaml.cs
+++ client/client/App.axaml.cs
@@ -15,22 +15,15 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainViewModel()
-            };
+            desktop.MainWindow = new MainWindow();
         }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-        {
-            singleViewPlatform.MainView = new MainView
-            {
-                DataContext = new MainViewModel()
-            };
-        }
+        
 
         base.OnFrameworkInitializationCompleted();
+        
     }
 
 }
