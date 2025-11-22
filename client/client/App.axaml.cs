ded411446516b8738d744ce2bdfb0dbb0ad9f5ab
Yupi <youngperchik2404@gmail.com>
2025-11-22 11:46:36 +0500

    создан класс навигации и зарегистрирован #10

--- client/client/App.axaml.cs
+++ client/client/App.axaml.cs
@@ -1,13 +1,23 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using client.Services;
+using client.Services.Interfaces;
 using client.ViewModels;
 using client.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
 
 namespace client;
 
 public partial class App : Application
 {
+    private IServiceProvider? _serviceProvider;
+    public static IServiceProvider? ServiceProvider { get; private set; }
+
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -15,6 +25,9 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        var services = new ServiceCollection();
+        services.AddSingleton<INavigationService, NavigationService>();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
---
+++ client/client/Services/NavigationService.cs
@@ -0,0 +1,74 @@
+﻿using Avalonia.Controls;
+using client.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Services
+{
+    public class NavigationService : INavigationService, INotifyPropertyChanged
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly Stack<UserControl> _navigationStack;
+
+        private UserControl _currentUserControl;
+        public UserControl CurrentUserControl
+        {
+            get => _currentUserControl;
+            set
+            {
+                if (_currentUserControl == value) return;
+                _currentUserControl = value;
+                RaisePropertyChanged(nameof(CurrentUserControl));
+            }
+        }
+
+        public NavigationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _navigationStack = new Stack<UserControl>();
+        }
+
+        public bool CanGoBack => _navigationStack.Count > 0;
+
+        public void GoBack()
+        {
+            if (!CanGoBack)
+                return;
+
+            CurrentUserControl = _navigationStack.Pop();
+        }
+
+        public void NavigateTo<T>() where T : UserControl
+        {
+            NavigateTo(typeof(T));
+        }
+
+        public void NavigateTo(Type windowType)
+        {
+            if (!typeof(UserControl).IsAssignableFrom(windowType))
+                throw new ArgumentException("Type must be a UserControl");
+
+            if (CurrentUserControl != null)
+            {
+                _navigationStack.Push(CurrentUserControl);
+            }
+
+            CurrentUserControl = (UserControl)_serviceProvider.GetService(windowType);
+
+            if (CurrentUserControl == null)
+                throw new InvalidOperationException($"UserControl {windowType.Name} is not registered");
+        }
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
