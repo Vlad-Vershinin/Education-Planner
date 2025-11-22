2177c59d7fce1781136fa2b9f4f2cd0de78935bb
Yupi <youngperchik2404@gmail.com>
2025-11-22 18:18:01 +0500

    обновлены файлы дто на клиенте

---
+++ client/client/DTOs/LeveledSkillDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.DTOs
+{
+    public class LeveledSkillDTO
+    {
+        public SkillDTO Skill { get; set; } = new SkillDTO();
+        public int Level { get; set; }
+    }
+}
