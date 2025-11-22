477d9637d654f33d7c597e2fac64e2e3f38bd772
Dasadn0 <Yakaleks96@gmail.com>
2025-11-23 00:32:17 +0500

    SkillRepository

---
+++ server/server.Infrastucture/Repositories/SkillRepository.cs
@@ -0,0 +1,38 @@
+﻿using server.Domain.Interfaces.Repositories;
+using server.Domain.Models;
+using server.Persistence;
+using server.Persistence.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace server.Infrastucture.Repositories
+{
+    public class SkillRepository : ISkillRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public async Task<List<Skill>> GetAllSkills()
+        {
+            return _context.Skills.ToList();
+        }
+        public async Task<List<Skill>> GetSkillsByParent(int id)
+        {
+            return  _context.Skills.Where(x => x.ParentId == id).ToList();
+        }
+        public async Task<Skill> GetSkill(int id)
+        {
+            return _context.Skills.ElementAt(id);
+        }
+        public async Task<List<Course>> GetSkillCourses(int id)
+        {
+            return _context.Skills.ElementAt(id).Courses;
+        }
+        public async Task<List<Profession>> GetSkillProfession(int id)
+        {
+            return _context.Skills.ElementAt(id).Profession;
+        }
+    }
+}
