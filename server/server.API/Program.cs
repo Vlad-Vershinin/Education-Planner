4da88b165f73c039f20a4d781daac8bf80c6c90f
Vlad-Vershinin <vladversinin676@gmail.com>
2025-11-22 22:17:10 +0500

    db fix

--- server/server.API/Program.cs
+++ server/server.API/Program.cs
@@ -1,5 +1,8 @@
 
 
+using Microsoft.EntityFrameworkCore;
+using server.Persistence;
+
 namespace server.API
 {
     public class Program
@@ -7,7 +10,18 @@ namespace server.API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             var app = builder.Build();
+
+            app.UseRouting();
+            app.MapControllers();
+
+            app.Run();
         }
     }
 }
