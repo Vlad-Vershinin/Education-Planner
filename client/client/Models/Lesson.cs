45a0b645195b7794d4d4e357c716333a9022bd71
Yupi <youngperchik2404@gmail.com>
2025-11-22 12:23:10 +0500

    реализованы классы моделей

---
+++ client/client/Models/Lesson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Models
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Article> Content { get; set; } = new List<Article>();
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ client/client/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string LastName { get; set; }
+        public List<Course> EnrolledCourses { get; set; } = new List<Course>();
+        public string Login { get; set; }
+    }
+}
