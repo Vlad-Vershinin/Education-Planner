fcdb8b0a910a846801e0b946ccc4b56f1ffb75d7
Yupi <youngperchik2404@gmail.com>
2025-11-23 06:13:03 +0500

    реализован SkillViewModel, добавлена модель профессий

--- client/client/DTOs/SkillDTO.cs
+++ client/client/DTOs/SkillDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using client.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,6 @@ namespace client.DTOs
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; }
+        public List<Profession>? Professions { get; set; }
     }
 }
